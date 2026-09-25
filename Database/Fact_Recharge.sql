4f6a067042c470bc3b4748a66652d57105ed1b71
Balasurya Sivakumar <82596688+Balasuryadeveloper@users.noreply.github.com>
2026-09-25 18:23:23 +0530

    Optimize Fact_Recharge insertion using CTE

--- Database/Fact_Recharge.sql
+++ Database/Fact_Recharge.sql
@@ -1,51 +1,54 @@
-SET NOCOUNT ON;
+;WITH Numbers AS
+(
+    SELECT TOP (2000)
+           ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS N
+    FROM sys.all_objects a
+    CROSS JOIN sys.all_objects b
+)
+INSERT INTO Fact_Recharge
+(
+    SubscriberID,
+    PlanID,
+    RegionID,
+    RechargeDateID,
+    RechargeAmount
+)
+SELECT
+       DS.SubscriberID,
+       DS.PlanID,
+       DS.RegionID,
 
-DECLARE @Counter INT = 1;
+       DD.DateID,
 
-WHILE @Counter <= 2000
-BEGIN
+       CAST
+       (
+           CASE DS.PlanID
+                WHEN 1 THEN 199
+                WHEN 2 THEN 299
+                WHEN 3 THEN 499
+                WHEN 4 THEN 799
+                WHEN 5 THEN 399
+                WHEN 6 THEN 699
+                WHEN 7 THEN 999
+                WHEN 8 THEN 99
+                WHEN 9 THEN 149
+                WHEN 10 THEN 249
+                ELSE 199
+           END
+           +
+           (ABS(CHECKSUM(NEWID())) % 50)
+       AS DECIMAL(10,2))
 
-    INSERT INTO Fact_Recharge
-    (
-        SubscriberID,
-        PlanID,
-        RegionID,
-        RechargeDateID,
-        RechargeAmount
-    )
-    SELECT TOP 1
-           DS.SubscriberID,
-           DS.PlanID,
-           DS.RegionID,
+FROM Numbers N
 
-           (
-                SELECT TOP 1 DD.DateID
-                FROM Dim_Date DD
-                WHERE DD.FullDate >= DS.ActivationDate
-                ORDER BY NEWID()
-           ) AS RechargeDateID,
+INNER JOIN Dim_Subscriber DS
+    ON DS.SubscriberID =
+       ((N.N - 1) % (SELECT COUNT(*) FROM Dim_Subscriber)) + 1
 
-           CAST(
-                CASE DS.PlanID
-                    WHEN 1 THEN 199
-                    WHEN 2 THEN 299
-                    WHEN 3 THEN 499
-                    WHEN 4 THEN 799
-                    WHEN 5 THEN 399
-                    WHEN 6 THEN 699
-                    WHEN 7 THEN 999
-                    WHEN 8 THEN 99
-                    WHEN 9 THEN 149
-                    WHEN 10 THEN 249
-                    ELSE 199
-                END
-                +
-                (ABS(CHECKSUM(NEWID())) % 50)
-           AS DECIMAL(10,2))
-
-    FROM Dim_Subscriber DS
-    ORDER BY NEWID();
-
-    SET @Counter = @Counter + 1;
-
-END;
+CROSS APPLY
+(
+    SELECT TOP 1 DateID
+    FROM Dim_Date DD
+    WHERE DD.FullDate >= DS.ActivationDate
+    ORDER BY NEWID()
+) DD;
