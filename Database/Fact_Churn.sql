f403b58045d37a3ffa38b0a410c04f334a92299b
Balasurya Sivakumar <82596688+Balasuryadeveloper@users.noreply.github.com>
2026-09-25 18:43:21 +0530

    Refactor churn data insertion using CTE
    Replaced the WHILE loop with a CTE for inserting churn data.

--- Database/Fact_Churn.sql
+++ Database/Fact_Churn.sql
@@ -1,47 +1,44 @@
-SET NOCOUNT ON;
-
-DECLARE @Counter INT = 1;
-
-WHILE @Counter <= 120
-BEGIN
-
-    INSERT INTO Fact_Churn
-    (
-        SubscriberID,
-        RegionID,
-        ChurnDateID,
-        ChurnReason
-    )
-    SELECT TOP 1
+;WITH ChurnSubscribers AS
+(
+    SELECT TOP (120)
            DS.SubscriberID,
            DS.RegionID,
-
-           (
-                SELECT TOP 1 DD.DateID
-                FROM Dim_Date DD
-                WHERE DD.FullDate > DS.ActivationDate
-                ORDER BY NEWID()
-           ),
-
-           CASE ABS(CHECKSUM(NEWID())) % 8
-                WHEN 0 THEN 'Price Sensitivity'
-                WHEN 1 THEN 'Network Issues'
-                WHEN 2 THEN 'Better Competitor Offer'
-                WHEN 3 THEN 'Coverage Problems'
-                WHEN 4 THEN 'Poor Customer Service'
-                WHEN 5 THEN 'Low Usage'
-                WHEN 6 THEN 'Relocation'
-                ELSE 'Personal Reason'
-           END
-
+           DS.ActivationDate
     FROM Dim_Subscriber DS
-    WHERE DS.SubscriberID NOT IN
-    (
-        SELECT SubscriberID
-        FROM Fact_Churn
-    )
-    ORDER BY NEWID();
+    ORDER BY NEWID()
+)
+INSERT INTO Fact_Churn
+(
+    SubscriberID,
+    RegionID,
+    ChurnDateID,
+    ChurnReason
+)
+SELECT
+       CS.SubscriberID,
+       CS.RegionID,
 
-    SET @Counter += 1;
+       DD.DateID,
 
-END;
+       CASE ABS(CHECKSUM(NEWID())) % 8
+            WHEN 0 THEN 'Price Sensitivity'
+            WHEN 1 THEN 'Network Issues'
+            WHEN 2 THEN 'Better Competitor Offer'
+            WHEN 3 THEN 'Coverage Problems'
+            WHEN 4 THEN 'Poor Customer Service'
+            WHEN 5 THEN 'Low Usage'
+            WHEN 6 THEN 'Relocation'
+            ELSE 'Personal Reason'
+       END
+
+FROM ChurnSubscribers CS
+
+CROSS APPLY
+(
+    SELECT TOP 1 DD.DateID
+    FROM Dim_Date DD
+    WHERE DD.FullDate >= CS.ActivationDate
+      AND DD.FullDate <= '2026-09-30'
+    ORDER BY NEWID()
+) DD;
+``
